75e0530a14d2f79e0cac36df4db03b318b61012d
firatkill <firatkill@icloud.com>
2022-06-04 20:35:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,87 @@
-let player1 = choose();
-let player2 = chooseLeft();
-let currentPlayer = player1;
-let span1 = document.getElementById("1").textContent;
-let span2 = document.getElementById("2").textContent;
-let span3 = document.getElementById("3").textContent;
-let span4 = document.getElementById("4").textContent;
-let span5 = document.getElementById("5").textContent;
-let span6 = document.getElementById("6").textContent;
-let span7 = document.getElementById("7").textContent;
-let span8 = document.getElementById("8").textContent;
-let span9 = document.getElementById("9").textContent;
+let arr = [
+  `${document.getElementById(1).textContent}`,
+  `${document.getElementById(2).textContent}`,
+  `${document.getElementById(3).textContent}`,
+  `${document.getElementById(4).textContent}`,
+  `${document.getElementById(5).textContent}`,
+  `${document.getElementById(6).textContent}`,
+  `${document.getElementById(7).textContent}`,
+  `${document.getElementById(8).textContent}`,
+  `${document.getElementById(9).textContent}`,
+];
+let winningArr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+  [1, 4, 7],
+  [3, 6, 9],
+  [2, 5, 8],
+];
 
-document.querySelector(".grid-container").onclick = (e) => {
-  e.target.firstElementChild.textContent = `${currentPlayer}`;
-  controller();
-  nextPlayer();
-};
+// Player codes
 
-function controller(x, y, z) {
-  if (control(span1, span2, span3)) {
-    won();
-  } else if (control(span4, span5, span6)) {
-    won();
-  } else if (control(span7, span8, span9)) {
-    won();
-  }
-}
-function won() {
-  console.log("you won");
-}
-function choose() {
-  if (confirm("X or O") == true) {
-    return "X";
-  } else {
-    return "O";
-  }
-}
+let currentPlayer;
+let player1 = "X";
+let player2 = "O";
+currentPlayer = player2;
 
-function nextPlayer() {
-  if (currentPlayer == player1) {
-    currentPlayer = player2;
-  } else {
-    currentPlayer = player1;
-  }
-}
-function chooseLeft() {
-  if (player1 == "X") {
-    return "O";
-  } else {
-    return "X";
-  }
-}
+// Event Listeners
+document.addEventListener("DOMContentLoaded", selection);
 
-function control(x, y, z) {
-  if (x != "") {
-    if (x === y && x === z) {
-      return true;
+document.querySelector(".grid-container").addEventListener("click", (e) => {
+  if (e.target.className == "grid-item") {
+    e.target.firstElementChild.textContent = currentPlayer;
+    attach();
+    controller();
+    if (currentPlayer == player1) {
+      currentPlayer = player2;
     } else {
-      return false;
+      currentPlayer = player1;
     }
+  }
+});
+
+// functions
+
+function selection() {
+  if (confirm("Press Ok for X, cancel for O.")) {
+    currentPlayer = player1;
+  } else {
+    currentPlayer = player2;
+  }
+}
+
+function attach() {
+  for (let i = 1; i < 10; i++) {
+    arr[i - 1] = document.getElementById(`${i}`).textContent;
+  }
+}
+
+function controller() {
+  for (let i = 0; i < winningArr.length; i++) {
+    if (control(winningArr[i])) {
+      alert(`'${currentPlayer}' has WON!!`);
+      reset();
+    }
+  }
+}
+function control(arg) {
+  if (arr[arg[0] - 1] != "" && arr[arg[2] - 1] != "" && arr[arg[1] - 1] != "") {
+    if (
+      arr[arg[0] - 1] == arr[arg[1] - 1] &&
+      arr[arg[0] - 1] == arr[arg[2] - 1]
+    ) {
+      return true;
+    }
+  }
+}
+
+function reset() {
+  arr = ["", "", "", "", "", "", "", "", ""];
+  for (let i = 0; i < 10; i++) {
+    arr[i] = "";
+    document.getElementById(`${i + 1}`).textContent = "";
   }
 }
